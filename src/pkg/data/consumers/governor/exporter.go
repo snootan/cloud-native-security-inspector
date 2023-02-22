e97a26908cce14109259c27c51b5a3632b13443a
Nootan Singh <snootan@vmware.com>
2023-02-22 09:37:12 +0530

    Added UTs and addressed comments
    Signed-off-by: Nootan Singh <snootan@vmware.com>

--- src/pkg/data/consumers/governor/exporter.go
+++ src/pkg/data/consumers/governor/exporter.go
@@ -3,7 +3,9 @@ package consumers
 import (
 	"context"
 	"errors"
+	"fmt"
 	api "github.com/vmware-tanzu/cloud-native-security-inspector/src/api/v1alpha1"
+	"github.com/vmware-tanzu/cloud-native-security-inspector/src/lib/log"
 	openapi "gitlab.eng.vmware.com/vac/catalog-governor/api-specs/catalog-governor-service-rest/go-client"
 	"net/http"
 )
@@ -17,35 +19,46 @@ type GovernorExporter struct {
 
 // SendReportToGovernor is used to send report to governor url http end point.
 func (g GovernorExporter) SendReportToGovernor() error {
-	// Get api request model from assessment report.
-	kubernetesCluster := getAPIRequest(*g.Report)
+	// Get governor api request model from assessment report.
+	kubernetesCluster := getGovernorAPIPayload(*g.Report)
 
 	// Create api client to governor api.
-	apiClient := openapi.NewAPIClient(openapi.NewConfiguration())
-	apiSaveClusterRequest := apiClient.ClustersApi.UpdateTelemetry(context.Background(), g.ClusterID)
+	config := openapi.NewConfiguration()
+
+	// Appending api URL in configuration.
+	config.Servers = openapi.ServerConfigurations{{
+		URL: g.ApiURL,
+	}}
+	
+	apiClient := openapi.NewAPIClient(config)
+	apiSaveClusterRequest := apiClient.ClustersApi.UpdateTelemetry(context.Background(), g.ClusterID).KubernetesTelemetryRequest(kubernetesCluster)
 
 	// Call api cluster to send telemetry data and get response.
-	response, err := apiSaveClusterRequest.KubernetesTelemetryRequest(kubernetesCluster).Execute()
+	response, err := apiClient.ClustersApi.UpdateTelemetryExecute(apiSaveClusterRequest)
 	if err != nil {
+		log.Errorf("Governor api response error: %v", err)
 		return err
 	}
 	if response.StatusCode != http.StatusOK {
-		return errors.New(response.Status)
+		log.Errorf("Governor api response status: %v", response.StatusCode)
+		return errors.New(fmt.Sprintf("Governor api response status: %s", response.Status))
 	}
 
 	return nil
 }
 
-// getAPIRequest is used to map assessment report to client model.
-func getAPIRequest(doc api.AssessmentReport) openapi.KubernetesTelemetryRequest {
+// getGovernorAPIPayload is used to map assessment report to client model.
+func getGovernorAPIPayload(doc api.AssessmentReport) openapi.KubernetesTelemetryRequest {
 	kubernetesCluster := openapi.NewKubernetesTelemetryRequestWithDefaults()
 	for _, nsa := range doc.Spec.NamespaceAssessments {
 		for _, workloadAssessment := range nsa.WorkloadAssessments {
 			kubernetesWorkloads := openapi.NewKubernetesWorkloadWithDefaults()
 			kubernetesWorkloads.Name = workloadAssessment.Workload.Name
 			kubernetesWorkloads.Kind = workloadAssessment.Workload.Kind
-			kubernetesWorkloads.Namespace = nsa.Namespace.Name
-			kubernetesWorkloads.Replicas = *workloadAssessment.Workload.Replicas.Spec.Replicas
+			if nsa.Namespace.Name != "" {
+				kubernetesWorkloads.Namespace = nsa.Namespace.Name
+			}
+			kubernetesWorkloads.Replicas = workloadAssessment.Workload.Replicas
 			for _, pod := range workloadAssessment.Workload.Pods {
 				containerData := openapi.NewContainerWithDefaults()
 				for _, container := range pod.Containers {
